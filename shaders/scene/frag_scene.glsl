16ad40c6d09822e00166475387799f46c69b7962
Rockyroad14 <jaredreich12@gmail.com>
2023-10-29 23:23:00 -0400

    shadowmap

---
+++ shaders/scene/frag_scene.glsl
@@ -0,0 +1,14 @@
+#version 420 core
+
+// todo: define all the input variables to the fragment shader
+
+
+// todo: define all the uniforms
+
+
+layout (binding=0) uniform sampler2D depthTex;  // depth texture bound to texture unit 0
+out vec4 outColor;
+
+void main(){
+    // todo: fill in the fragment shader
+}
---
+++ shaders/scene/vert_scene.glsl
@@ -0,0 +1,18 @@
+#version 420 core
+
+// Attributes
+layout (location = 0) in vec3 position;    // we can also use layout to specify the location of the attribute
+layout (location = 1) in vec2 uv;
+layout (location = 2) in vec3 normal;
+
+
+// todo: define all the out variables
+
+
+// todo: define all the uniforms
+
+
+
+void main(){
+    // todo: fill in vertex shader
+}
