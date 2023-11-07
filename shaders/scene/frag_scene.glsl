f44f73ea4af1b0b4d121edb899e49c71832c703c
Jared Reich <ja548470@ucf.edu>
2023-11-07 17:14:59 -0500

    changes

--- shaders/scene/frag_scene.glsl
+++ shaders/scene/frag_scene.glsl
@@ -4,15 +4,32 @@
 in vec3 fragPos;
 in vec3 fragNormal;
 in vec3 viewVector;
-in vec4 fragPosLightSpace;
+
 
 // todo: define all the uniforms
 uniform vec3 material_color;
 uniform vec3 light_pos;
-
+uniform vec3 viewVector;
+uniform vec3 lightColor;
 
  // depth texture bound to texture unit 0
 out vec4 outColor;
+
+vec3 ambientColor()
+{
+    return ambientIntensity * material_color;
+}
+
+vec3 specularColor()
+{
+
+}
+
+vec3 frensel(vec3 V, vec3 H)
+{
+    vec3 F0 = material_color;
+    return F0 + (1 - F0) * (1 - dot(V, H));
+}
 
 
 
